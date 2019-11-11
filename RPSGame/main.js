7c1feec38a10764518b78e6ef8f207bbe1110a95
Andrei Oprea <andrei.oprea91@yahoo.com>
2019-11-11 15:41:35 +0200

    Update main.js

--- RPSGame/main.js
+++ RPSGame/main.js
@@ -1,15 +1,13 @@
-
 const rock= document.getElementById("rock");
 const paper= document.getElementById("paper");
 const scissors= document.getElementById("scissors");
-let userScoreBoard=document.getElementById('userscore');
-let computerScoreBoard=document.getElementById('computerscore');
+const userScoreBoard=document.getElementById('userscore');
+const computerScoreBoard=document.getElementById('computerscore');
+const result=document.getElementById('result');
 let userScore=0;
 let computerScore=0;
 
-
 function getComputerChoice() {
-
  let randomNumber= Math.floor(Math.random()*3);
   switch (randomNumber){
     case 0:
@@ -24,51 +22,49 @@ function getComputerChoice() {
   }
 }
 
-function win(){
-  console.log("win")
+function win(userChoice,computerChoice){
   userScore++;
   userScoreBoard.innerHTML=userScore;
+result.innerHTML=` ${userChoice} beats ${computerChoice}. You WIN!`;
 }
 
-function lose(){
-  console.log("lose")
+function lose(userChoice,computerChoice){
   computerScore++;
   computerScoreBoard.innerHTML=computerScore;
+  result.innerHTML=` ${userChoice} beats ${computerChoice}. You LOOSE!`;
 }
 
-function draw(){
-  console.log("draw")
+function draw(userChoice,computerChoice){
+  result.innerHTML=` ${userChoice} equal ${computerChoice}. DRAW!`;
 }
 
 function determineWinner(userChoice){
   const computerChoice =getComputerChoice();
-  console.log('You threw: ' + userChoice);
-  console.log('The computer threw:' + computerChoice);
    if (userChoice===computerChoice){
-      return draw();
+      return draw(userChoice,computerChoice);
     }
    if(userChoice==='rock'){
      if (computerChoice==='paper'){
-        return lose();
+        return lose(userChoice,computerChoice);
       }else{
-        return win();
+        return win(userChoice,computerChoice);
       }
-      }
+    }
    if(userChoice==='paper'){
      if(computerChoice==='scissors'){
-        return lose();
+        return lose(userChoice,computerChoice);
       }else {
-        return win();
+        return win(userChoice,computerChoice);
       }
     }
     if (userChoice==='scissors'){
      if(computerChoice==='rock'){
-        return lose();
+        return lose(userChoice,computerChoice);
       }else{
-        return win();
+        return win(userChoice,computerChoice);
       }
-    }
-  }
+     }
+ }
 
 rock.addEventListener('click', function(){
   return determineWinner('rock');
